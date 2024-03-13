99886aa0c914986ac252598bfdebe7099bcab2b0
BeneathTheManWeFindHis-Nucleus <benfarley4@gmail.com>
2024-03-12 23:23:25 -0600

    request change to interface arguments

--- chebyshev.py
+++ chebyshev.py
@@ -5,12 +5,16 @@ using Chebyshev methods in part a)."""
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import time
 from scipy.special import roots_chebyt
 from numpy.polynomial import Chebyshev as T
 
 #Define functions
 def f(x):                         #Function to be approximated
     return np.sin(x)
+
+a = -np.pi                     #Determine domain
+b = np.pi
 
 def coefficients(N,k,j,f):      #Find coefficients
     coeffs = []
@@ -78,9 +82,11 @@ plt.ylabel('f(x)')
 plt.title('Derivative of Chebyshev Interpolation for Sin(x)')
 plt.grid(True)
 plt.show() 
+print()
 
-def chebyshev(f):
-    n = 30
+def chebyshev(f,a,b):
+    start_time = time.time()
+    n = 100
     N = n + 1
    
     k = np.arange(N)               #Find coeffiecients
@@ -94,10 +100,7 @@ def chebyshev(f):
         c_j = c_j * (2 / N)
         coeffs.append(c_j)
         
-    roots, _ = roots_chebyt(n)     #Find roots
-    
-    a = -np.pi                     #Determine domain
-    b = np.pi
+    roots, _ = roots_chebyt(n)        #Find roots, determine domain
     x = (roots + 1) * (b - a) / 2 + a
     
     p = T(coeffs)                  #Determine Chebyshev polynomial and first derivative
@@ -106,6 +109,7 @@ def chebyshev(f):
     f_cheby_prime = p_prime(roots)
     
     mean_deriv_error = abs(np.sum(np.gradient(f(roots),roots) - f_cheby_prime)/n)   #Determine error
+    #mean_deriv_error = abs(np.sum(np.cos(roots) - f_cheby_prime)/n) If true derivative is known, plug it in
     print(f"The mean error in the derivative of the {n}-order Chebyshev approximation is: {mean_deriv_error}.")
 
     #Plot the Chebyshev derivative vs the actual derivative
@@ -113,13 +117,18 @@ def chebyshev(f):
     plt.plot(x, f_cheby_prime, marker='o', linestyle = '-', label='Chebyshev Derivative Approximation')
     plt.plot(x, np.gradient(f(roots),roots), linestyle='--',label=f'Derivative of Arbitrary Function: f(x)')
     plt.legend()
-    plt.xticks(np.arange(-np.pi, np.pi+0.1, np.pi/2), [r'$-\pi$', r'$-\pi/2$', '0', r'$\pi/2$', r'$\pi$'])
     plt.xlabel('x')
     plt.ylabel('y')
     plt.title('Derivative of Chebyshev Interpolation for Arbitrary Function')
     plt.grid(True)
+    
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print("Execution time:", execution_time, "seconds")
+    
     plt.show() 
     
     return f_cheby_prime
-    
-cheb = chebyshev(f)
+   
+cheb = chebyshev(f,a,b)
+
--- interface.py
+++ interface.py
@@ -8,7 +8,10 @@ def derivative(f, x, method):
     elif method == 'Dual':
         return dual(f, x)
     elif method == 'Chebyshev':
-        return chebyshev(f, x)
+        return chebyshev(f, a, b)
     else:
         raise ValueError('Invalid method')
     
+""" For Chebyshev polynomials, the domain has to be defined as a transformation of 
+the roots of the polynomials from [-1,1] to endpoints [a,b]. Could we define our
+interface to accept the endpoints as arguments (f, a, b) to accomodate this?
