52dfab45682eda5eadb93ee344fa06758bf9a3d2
BeneathTheManWeFindHis-Nucleus <benfarley4@gmail.com>
2024-03-12 21:52:09 -0600

    Made it so the interface will call a function that works

--- chebyshev.py
+++ chebyshev.py
@@ -9,16 +9,16 @@ from scipy.special import roots_chebyt
 from numpy.polynomial import Chebyshev as T
 
 #Define functions
-def y(x):               #Function to be approximated
+def f(x):                         #Function to be approximated
     return np.sin(x)
 
-def coefficients(N,k,j,y):      #Find coefficients
+def coefficients(N,k,j,f):      #Find coefficients
     coeffs = []
     for j_val in j:
         c_j = 0
         for k_val in k:
             eval_at = np.cos((np.pi*(k_val+.5))/N)
-            c_j += y(eval_at)*(np.cos((np.pi*j_val*(k_val+.5))/N))
+            c_j += f(eval_at)*(np.cos((np.pi*j_val*(k_val+.5))/N))
         c_j = c_j * (2 / N)
         coeffs.append(c_j)
         print(f"c{j_val} = {c_j}") 
@@ -26,7 +26,7 @@ def coefficients(N,k,j,y):      #Find coefficients
 
 #Initialize everything
 n = 11                  #Order of the Chebyshev polynomial
-N = n + 1              #Number of coefficients needed
+N = n + 1               #Number of coefficients needed
 k = np.arange(N)
 j = np.arange(N)
 print()
@@ -38,7 +38,7 @@ print()
 
 #Find coefficients
 print(f"The coefficients of the {n}-order polynomial are:")
-coeffs = coefficients(N,k,j,y)
+coeffs = coefficients(N,k,j,f)
 print()
 # Define the interval
 a = -np.pi
@@ -47,34 +47,79 @@ x = (roots + 1) * (b - a) / 2 + a   # Map roots from [-1, 1] to [-π, π]
 
 #Generate polynomial and evaluate it at the roots     
 p = T(coeffs)
-y_cheby = p(roots) 
+f_cheby = p(roots) 
 
 #Plot the approximation
 plt.figure(figsize=(10, 6))
-plt.plot(x, y_cheby, marker='o', linestyle = '-', label='Chebyshev Approximation')
+plt.plot(x, f_cheby, marker='o', linestyle = '-', label='Chebyshev Approximation')
 plt.plot(x, np.sin(roots), linestyle='--',label='Sine Function')
 plt.legend()
 plt.xticks(np.arange(-np.pi, np.pi+0.1, np.pi/2), [r'$-\pi$', r'$-\pi/2$', '0', r'$\pi/2$', r'$\pi$'])
 plt.xlabel('x')
-plt.ylabel('y')
+plt.ylabel('f(x)')
 plt.title('Chebyshev Interpolation for Sin(x)')
 plt.grid(True)
 plt.show()
 
 #Calculate the derivative of the approximation and its error
 p_prime = p.deriv()
-y_cheby_prime = p_prime(roots)
-mean_deriv_error = np.sum(np.cos(roots) - y_cheby_prime)/(len(roots))
+f_cheby_prime = p_prime(roots)
+mean_deriv_error = abs(np.sum(np.cos(roots) - f_cheby_prime)/n)
 print(f"The mean error in the derivative of the {n}-order Chebyshev approximation is: {mean_deriv_error}.")
 
 #Plot the derivative of the approximation vs cos(x)
 plt.figure(figsize=(10, 6))
-plt.plot(x, y_cheby_prime, marker='o', linestyle = '-', label='Chebyshev Derivative Approximation')
+plt.plot(x, f_cheby_prime, marker='o', linestyle = '-', label='Chebyshev Derivative Approximation')
 plt.plot(x, np.cos(roots), linestyle='--',label='Cosine Function')
 plt.legend()
 plt.xticks(np.arange(-np.pi, np.pi+0.1, np.pi/2), [r'$-\pi$', r'$-\pi/2$', '0', r'$\pi/2$', r'$\pi$'])
 plt.xlabel('x')
-plt.ylabel('y')
+plt.ylabel('f(x)')
 plt.title('Derivative of Chebyshev Interpolation for Sin(x)')
 plt.grid(True)
 plt.show() 
+
+def chebyshev(f):
+    n = 30
+    N = n + 1
+   
+    k = np.arange(N)               #Find coeffiecients
+    j = np.arange(N) 
+    coeffs = []
+    for j_val in j:
+        c_j = 0
+        for k_val in k:
+            eval_at = np.cos((np.pi*(k_val+.5))/N)
+            c_j += f(eval_at)*(np.cos((np.pi*j_val*(k_val+.5))/N))
+        c_j = c_j * (2 / N)
+        coeffs.append(c_j)
+        
+    roots, _ = roots_chebyt(n)     #Find roots
+    
+    a = -np.pi                     #Determine domain
+    b = np.pi
+    x = (roots + 1) * (b - a) / 2 + a
+    
+    p = T(coeffs)                  #Determine Chebyshev polynomial and first derivative
+    f_cheby = p(roots)
+    p_prime = p.deriv()
+    f_cheby_prime = p_prime(roots)
+    
+    mean_deriv_error = abs(np.sum(np.gradient(f(roots),roots) - f_cheby_prime)/n)   #Determine error
+    print(f"The mean error in the derivative of the {n}-order Chebyshev approximation is: {mean_deriv_error}.")
+
+    #Plot the Chebyshev derivative vs the actual derivative
+    plt.figure(figsize=(10, 6))
+    plt.plot(x, f_cheby_prime, marker='o', linestyle = '-', label='Chebyshev Derivative Approximation')
+    plt.plot(x, np.gradient(f(roots),roots), linestyle='--',label=f'Derivative of Arbitrary Function: f(x)')
+    plt.legend()
+    plt.xticks(np.arange(-np.pi, np.pi+0.1, np.pi/2), [r'$-\pi$', r'$-\pi/2$', '0', r'$\pi/2$', r'$\pi$'])
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Derivative of Chebyshev Interpolation for Arbitrary Function')
+    plt.grid(True)
+    plt.show() 
+    
+    return f_cheby_prime
+    
+cheb = chebyshev(f)
