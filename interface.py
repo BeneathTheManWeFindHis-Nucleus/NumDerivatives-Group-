4c2bed072f41ac530e6bef944788e7332573e70b
BlakePR <BlakePRomrell@gmail.com>
2024-03-13 08:40:16 -0600

    response

--- interface.py
+++ interface.py
@@ -8,10 +8,13 @@ def derivative(f, x, method):
     elif method == 'Dual':
         return dual(f, x)
     elif method == 'Chebyshev':
-        return chebyshev(f, a, b)
+        return chebyshev(f, x)
     else:
         raise ValueError('Invalid method')
     
 """ For Chebyshev polynomials, the domain has to be defined as a transformation of 
 the roots of the polynomials from [-1,1] to endpoints [a,b]. Could we define our
 interface to accept the endpoints as arguments (f, a, b) to accomodate this? """
+# you could do this without changing the interface, by grabbing x[0] and x[-1] and using them as a and b
+# also, we don't want your method to plot things, just to return the values of the derivative at the points
+# and let main.ipynb do the plotting
